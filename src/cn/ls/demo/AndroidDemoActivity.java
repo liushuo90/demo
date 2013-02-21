15175aaf612efee44d476d24a18947f057881a3a
liushuo <liushuo90@hotmail.com>
2013-02-21 16:11:50 +0800

    first commit

---
+++ src/cn/ls/demo/AndroidDemoActivity.java
@@ -0,0 +1,13 @@
+package cn.ls.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidDemoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
